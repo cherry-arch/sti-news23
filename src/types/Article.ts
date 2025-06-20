365dd2696900660e7e3c3039512b87f981074184
cherry-arch <charan555321@gmail.com>
2025-06-20 21:05:07 +0530

    Add files via upload

---
+++ src/types/Article.ts
@@ -0,0 +1,11 @@
+
+export interface Article {
+  id: string;
+  title: string;
+  description: string;
+  category: string;
+  imageUrl: string;
+  publishedAt: string;
+  source: string;
+  readTime: number;
+}
